ab57d9a85ace18d27b07c996d3caa7ddefd32a2f
cryptmask <emreozkurt1978@gmail.com>
2025-09-07 06:35:21 +0300

    feat: create HomePage with hero section and navigation

---
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header/Header";
+import Hero from "../../components/Hero/Hero";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+    </div>
+  );
+};
+
+export default HomePage;
